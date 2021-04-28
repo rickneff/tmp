bcda2f4356d13d0b72d934a4edae714ce1915243
Rick Neff <rickneff@users.noreply.github.com>
2021-04-28 14:34:13 -0600

    Update clj.py
    To make life easier.

--- clj.py
+++ clj.py
@@ -2,7 +2,7 @@ import subprocess
 
 def clj(clojure_program):
     with open('tmp.clj', 'w') as f:
-      f.write(clojure_program)
+      f.write('(print (do ' + clojure_program + '))')
     command = ['clojure', '-M', 'tmp.clj']
     output = subprocess.run(command, capture_output=True)
     if output.returncode:
