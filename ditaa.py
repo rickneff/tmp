41aaf6a2d7ff0f90562d0c9ecbab242a339eb049
Rick Neff <NeffR@byui.edu>
2021-04-28 18:42:00 -0600

    Really fix ditaa support.

--- ditaa.py
+++ ditaa.py
@@ -5,7 +5,7 @@ def ditaa(ditaa_str):
   with open('tmp.txt', 'w') as f:
     f.write(ditaa_str)
 
-  command = ['java', '-Dfile.encoding=UTF-8', '-jar', 'ditaa0_9.jar', '-E', '-o', 'tmp.txt', 'tmp.png')
+  command = ['java', '-Dfile.encoding=UTF-8', '-jar', 'ditaa0_9.jar', '-E', '-o', 'tmp.txt', 'tmp.png']
   output = subprocess.run(command, capture_output=True)
   if output.returncode:
     print(output.stderr.decode('utf-8'))
