912f13e81f7ae1dbdbe70edcc793b55b3c3ad634
Rick Neff <NeffR@byui.edu>
2021-04-28 18:39:16 -0600

    Fix ditaa support.

--- d
+++ d
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if ! test -f
+if ! test -f ditaa0_9.jar
 then
    curl -s -O https://rickneff.github.io/ditaa0_9.jar
    # allow easy import of ditaa Python function (see ditaa.py).
    ln -s c/ditaa.py
+   ln -s c/ditaa0_9.py
 fi
 exec java -jar ditaa0_9.jar "$@"
--- ditaa.py
+++ ditaa.py
@@ -1,16 +1,14 @@
 import subprocess
 from IPython.display import Image
 
-def ditaa(ditaa_var):
-  ditaa_str = globals()[ditaa_var]
-  ditaa_png = ditaa_str + '.png'
+def ditaa(ditaa_str):
   with open('tmp.txt', 'w') as f:
     f.write(ditaa_str)
 
-  command = ['java', '-Dfile.encoding=UTF-8', '-jar', 'ditaa0_9.jar', '-E', '-o', 'tmp.txt', ditaa_png]
+  command = ['java', '-Dfile.encoding=UTF-8', '-jar', 'ditaa0_9.jar', '-E', '-o', 'tmp.txt', 'tmp.png')
   output = subprocess.run(command, capture_output=True)
   if output.returncode:
     print(output.stderr.decode('utf-8'))
   else:
     print(output.stdout.decode('utf-8'))
-  Image(ditaa_png)
+  Image('tmp.png')
